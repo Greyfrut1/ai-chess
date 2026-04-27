e935699526127a78b0844c72cf406c7bd1c43756
Greyfrut1 <vladchumachenko496@gmail.com>
2026-04-27 15:52:52 +0300

    Learn pytorch neural network. Start learn RL. Cart pol

--- ai-chess/tests/test_datasets_dataloader.py
+++ ai-chess/tests/test_datasets_dataloader.py
@@ -3,6 +3,7 @@ from torch.utils.data import Dataset
 from torchvision import datasets
 from torchvision.transforms import ToTensor
 import matplotlib.pyplot as plt
+from torch.utils.data import DataLoader
 
 
 training_data = datasets.FashionMNIST(
@@ -41,3 +42,15 @@ for i in range(1, cols * rows + 1):
     plt.axis("off")
     plt.imshow(img.squeeze(), cmap="gray")
 plt.show()
+
+train_dataloader = DataLoader(training_data, batch_size=64, shuffle=True)
+test_dataloader = DataLoader(test_data, batch_size=64, shuffle=True)
+
+train_features, train_labels = next(iter(train_dataloader))
+print(f"Feature batch shape: {train_features.size()}")
+print(f"Labels batch shape: {train_labels.size()}")
+img = train_features[0].squeeze()
+label = train_labels[0]
+plt.imshow(img, cmap="gray")
+plt.show()
+print(f"Label: {label}")
